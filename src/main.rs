5d6d6e28cf47f85e5b3907c6f8a9daa12b131dd3
tomyamkum <tomyamkum1090@gmail.com>
2019-09-21 04:55:50 +0900

    make 16bit register

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,20 @@ use make_cpu::comb_circ::*;
 use make_cpu::seq_circ::*;
 
 fn main() {
-	let mut reg = Reg::new(false);
-	println!("{}", reg.next(true, true));
-	println!("{}", reg.next(true, false));
-	println!("{}", reg.next(false, true));
-	println!("{}", reg.next(true, false));
-	println!("{}", reg.next(true, true));
+	let initial_value = vec![true,true,true,false,false,true,false,true,true,false,false,true,true,true,false,false];
+	let next1 = vec![true,true,false,false,false,true,false,true,true,false,false,true,true,true,false,false];
+	let next2 = vec![false,true,true,false,false,true,false,true,true,false,false,true,true,true,false,false];
+	let next3 = vec![true,false,true,false,false,true,false,true,true,false,false,true,true,true,false,false];
+	let mut reg16 = Reg16::new(initial_value);
+	let value = reg16.get();
+	println!("{:?}",value);
+	reg16.next(&next1, true);
+	let value = reg16.get();
+	println!("{:?}",value);
+	reg16.next(&next2, false);
+	let value = reg16.get();
+	println!("{:?}",value);
+	reg16.next(&next3, true);
+	let value = reg16.get();
+	println!("{:?}",value);
 }
--- src/seq_circ.rs
+++ src/seq_circ.rs
@@ -12,8 +12,60 @@ impl Reg {
 			 dff: Dff::new(initial_value),
 		}
 	}
-	pub fn next(&mut self, input: bool, load: bool) -> bool {
+	pub fn next(&mut self, input: bool, load: bool) {
 		self.dff.pre_value = mux(self.dff.pre_value, input, load);
+	}
+	pub fn get(&self) -> bool {
 		self.dff.pre_value
+	}
+}
+
+pub struct Reg16 {
+	pub reg: Vec<Reg>,
+}
+
+impl Reg16 {
+	pub fn new(initial_value: Vec<bool>) -> Reg16 {
+		Reg16 {
+			reg: vec![Reg::new(initial_value[0]), Reg::new(initial_value[1]), Reg::new(initial_value[2]), Reg::new(initial_value[3]), Reg::new(initial_value[4]), Reg::new(initial_value[5]), Reg::new(initial_value[6]), Reg::new(initial_value[7]), Reg::new(initial_value[8]), Reg::new(initial_value[9]), Reg::new(initial_value[10]), Reg::new(initial_value[11]), Reg::new(initial_value[12]), Reg::new(initial_value[13]), Reg::new(initial_value[14]), Reg::new(initial_value[15])],
+		}
+	}
+	pub fn next(&mut self, input: &Vec<bool>, load: bool) {
+		self.reg[0].dff.pre_value = mux(self.reg[0].dff.pre_value, input[0], load);
+		self.reg[1].dff.pre_value = mux(self.reg[1].dff.pre_value, input[1], load);
+		self.reg[2].dff.pre_value = mux(self.reg[2].dff.pre_value, input[2], load);
+		self.reg[3].dff.pre_value = mux(self.reg[3].dff.pre_value, input[3], load);
+		self.reg[4].dff.pre_value = mux(self.reg[4].dff.pre_value, input[4], load);
+		self.reg[5].dff.pre_value = mux(self.reg[5].dff.pre_value, input[5], load);
+		self.reg[6].dff.pre_value = mux(self.reg[6].dff.pre_value, input[6], load);
+		self.reg[7].dff.pre_value = mux(self.reg[7].dff.pre_value, input[7], load);
+		self.reg[8].dff.pre_value = mux(self.reg[8].dff.pre_value, input[8], load);
+		self.reg[9].dff.pre_value = mux(self.reg[9].dff.pre_value, input[9], load);
+		self.reg[10].dff.pre_value = mux(self.reg[10].dff.pre_value, input[10], load);
+		self.reg[11].dff.pre_value = mux(self.reg[11].dff.pre_value, input[11], load);
+		self.reg[12].dff.pre_value = mux(self.reg[12].dff.pre_value, input[12], load);
+		self.reg[13].dff.pre_value = mux(self.reg[13].dff.pre_value, input[13], load);
+		self.reg[14].dff.pre_value = mux(self.reg[14].dff.pre_value, input[14], load);
+		self.reg[15].dff.pre_value = mux(self.reg[15].dff.pre_value, input[15], load);
+	}
+	pub fn get(&self) -> Vec<bool> {
+		let mut ans = Vec::new();
+		ans.push(self.reg[0].dff.pre_value);
+		ans.push(self.reg[1].dff.pre_value);
+		ans.push(self.reg[2].dff.pre_value);
+		ans.push(self.reg[3].dff.pre_value);
+		ans.push(self.reg[4].dff.pre_value);
+		ans.push(self.reg[5].dff.pre_value);
+		ans.push(self.reg[6].dff.pre_value);
+		ans.push(self.reg[7].dff.pre_value);
+		ans.push(self.reg[8].dff.pre_value);
+		ans.push(self.reg[9].dff.pre_value);
+		ans.push(self.reg[10].dff.pre_value);
+		ans.push(self.reg[11].dff.pre_value);
+		ans.push(self.reg[12].dff.pre_value);
+		ans.push(self.reg[13].dff.pre_value);
+		ans.push(self.reg[14].dff.pre_value);
+		ans.push(self.reg[15].dff.pre_value);
+		ans
 	}
 }
