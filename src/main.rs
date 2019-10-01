9cffc2df12f3aed8bd04d339b71d4fc55806b82e
tomyamkum <tomyamkum1090@gmail.com>
2019-10-01 21:56:52 +0900

    hoge

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use make_cpu::computer::*;
-use make_cpu::seq_circ::*;
 
 fn main() {
 	let mut computer = Computer::new();
 	computer.load("test.txt");
-	println!("{:?}",computer.exec(false));
+	computer.exec(false);
 }
